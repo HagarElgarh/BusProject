e4ad7290886470c6681b6bf16e23c28e8e32a01d
hagarelgarh98 <hagar.elgarh98@gmail.com>
2020-11-22 14:15:00 +0400

    gh

--- BusProject/Bus.cs
+++ BusProject/Bus.cs
@@ -8,11 +8,103 @@ namespace BusProject
 {
     class Bus
     {
-        public static object[,] BusTimeLine = new object[12, 2] { { 1,"7:00 AM" }, { 2, "8:00 AM" }, { 3, "9:00 AM" } ,
-                                                                 { 1,"10:00 AM"}, { 2, "11:00 AM"}, { 3, "12:00 AM" } ,
+
+        // table 1 
+        public static object[,] BusTimeLine = new object[12, 2] {{ 1,"7:00 AM" }, { 2, "8:00 AM" }, { 3, "9:00 AM" } ,
+                                                                 { 1,"10:00 AM"}, { 2, "11:00 AM"}, { 3, "12:00 AM"} ,
                                                                  { 1,"1:00 PM" }, { 2, "2:00 PM" }, { 3, "3:00 PM" },
                                                                  { 1,"4:00 PM" }, { 2, "5:00 PM" }, { 3, "6:00 PM" }};
 
+
+
+        /// table 2
+
+
+        public static object[,] BusTimeLine2 = new object[12, 2]{{ 4,"7:00 AM" }, { 5, "8:00 AM" }, {6, "9:00 AM" } ,
+                                                                 { 4,"10:00 AM"}, { 5, "11:00 AM"}, {6, "12:00 AM"} ,
+                                                                 { 4,"1:00 PM" }, { 5, "2:00 PM" }, {6, "3:00 PM" },
+                                                                 { 4,"4:00 PM" }, { 5, "5:00 PM" }, {6, "6:00 PM" }};
+
+        // dispaly Table 1 
+        public void DisplayTime2()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+
+                for (int j = 0; j < 2; j += 2)
+                {
+
+
+
+                    Console.WriteLine("Bus ID :{0} ---- Time : {1} ", BusTimeLine2[i, j], BusTimeLine2[i, j + 1]);
+
+
+                }
+            }
+
+
+        }
+
+        // Display table 2
+        public void DisplayTime1()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+
+                for (int j = 0; j < 2; j += 2)
+                {
+
+
+
+                    Console.WriteLine("Bus ID :{0} ---- Time : {1} ", BusTimeLine2[i, j], BusTimeLine2[i, j + 1]);
+
+
+                }
+            }
+        }
+
+
+
+        // Select Bus ID based On time from table 1
+        public object SelectTime1(object x)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+
+                for (int j = 0; j < 2; j++)
+                {
+                    object xx;
+                    if (BusTimeLine2[i, j] == x)
+                    {
+                        xx = BusTimeLine2[i, j - 1];
+                        return xx;
+                        break;
+                    }
+                    //  Console.WriteLine("a[{0},{1}] = {2}", i, j, BusTimeLine2[i, j]);
+                }
+            }
+            return 0;
+        }
+
+        // Select Bus ID based On time from table 2
+        public object SelectTime2(object x)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+
+                for (int j = 0; j < 2; j++)
+                {
+                    object xx;
+                    if (BusTimeLine[i, j] == x)
+                    {
+                        xx = BusTimeLine[i, j - 1];
+                        return xx;
+                        break;
+                    }
+                }
+            }
+            return 0;
+        }
         /*
          *  id  s  As
              0  1   2
@@ -52,10 +144,16 @@ namespace BusProject
             return BusSeats;
         }
 
-        object[,] BusTime()
+        object[,] BusTime1()
         {
 
             return BusTimeLine;
+        }
+
+        object[,] BusTime2()
+        {
+
+            return BusTimeLine2;
         }
     }
 }
